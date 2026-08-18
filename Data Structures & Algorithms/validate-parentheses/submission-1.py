f16c2a86a423fde69cb3828dfbcbc6e8b5a41b79
Ariq Isaac <2009ariq@gmail.com>
2026-08-18 11:20:00 +0800

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parenthesis = ["()", "[]", "{}"]
+        output = s
+        while output:
+            if parenthesis[0] in output:
+                output = output.replace(parenthesis[0], "")
+            elif parenthesis[1] in output:
+                output = output.replace(parenthesis[1], "")
+            elif parenthesis[2] in output:
+                output = output.replace(parenthesis[2], "")
+            else:
+                break
+        return True if not output else False
